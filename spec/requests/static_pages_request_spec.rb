c654836b34ccf87e9abcb5d02caf99c2693b2d63
micahabanschick <micha.ban@gmail.com>
2021-01-25 16:14:51 -0500

    general cleanup

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
-  describe "GET /welcome" do
+  describe "GET /" do
     it "returns http success" do
-      get "/static_pages/welcome"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
