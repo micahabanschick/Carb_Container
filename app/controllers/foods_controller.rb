780b2a86d2b39a63a05dcb37d679b34a57cdf7ac
micahabanschick <micha.ban@gmail.com>
2021-01-26 16:38:10 -0500

    FoodsController(updated)

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,48 @@
 class FoodsController < ApplicationController
-end
+    def index
+      @foods = Food.all
+    end
+  
+    def show
+      @food = Food.find_by(id: params[:id])
+    end
+  
+    def new
+      @food = Food.new
+    end
+  
+    def create
+      food = food.create(food_params)
+      redirect_to food_path(food)
+    end
+  
+    def edit
+      @food = Food.find_by(id: params[:id])
+    end
+  
+    def update
+      food = Food.find_by(id: params[:id])
+      food.update(food_params)
+      redirect_to food_path(food)
+    end
+
+    def destroy
+        food = Food.find_by(id: params[:id])
+        food.delete
+        redirect_to root_path
+    end
+  
+    private
+    def food_params
+      params.require(:food).permit(
+          :name,
+          :brand,
+          :carb_count,
+          :fiber_count,
+          :protein_count,
+          :fat_count,
+          :calorie_count
+        )
+    end
+  end 
+  
