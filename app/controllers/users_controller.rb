fd3decc8b3b6d3a19d3e721e2eaf8a471a0e9788
micahabanschick <micha.ban@gmail.com>
2021-02-25 13:44:35 -0500

    form validation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
     end 
 
     def create
-      user = User.new(user_params)
-      if user.save
-        session[:user_id] = user.id
-        redirect_to user_path(user)
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
       else
         render 'new'
       end
